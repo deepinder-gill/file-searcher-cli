cf25095538df7cb49fa7cf898911f7059b558fab
deepinder-gill <deepinderrgilll@gmail.com>
2026-02-13 04:24:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import os
 
-class file_searching():
-  def ask_user(self):
+class FileSearching():
+  def AskUser(self):
     while True:
-      asking = int(input("1. Search 2. Exit [1/2]"))
       try:
+        asking = int(input("1. Search 2. Exit [1/2]"))
         if asking == 1:
           folder_path = input("Enter Folder Path: ")
           keyword = input("Enter Keyword:")
@@ -13,34 +13,40 @@ class file_searching():
           print("Searching...")
 
           if os.path.exists(folder_path):
+            repetition = 0
             for root, subfolders, files in os.walk(folder_path):
-              repetition = 0
               for file in files:
+
                 if extension and os.path.splitext(file)[1].lower() != extension:
                   continue
                 file_path = os.path.join(root, file)
                 try:          
-                  with open(file_path, encoding ="utf-18" ) as read_file:
+                  with open(file_path, "r", encoding ="utf-8" ) as read_file:
                     for i, line in enumerate(read_file, start = 1):
-                      if keyword in line:
+                      if keyword.lower().strip() in line.lower():
                         repetition += 1
-                        print(f"keyword found at:\n{i}\nin line:{line}\n")
+                        print("[FILE NAME MATCH]")
+                        print(f"File: {file_path}\nLine index: {i}\nLine: {line}")
                 except Exception as e:
                   print(f"{file_path} not readable: {e}")
             print(f"----------------------------------------\nNo. of result found: {repetition}\n----------------------------------------")
-            break
-          elif asking == 2:
-            return asking
+
+          elif not os.path.isdir(folder_path):
+            print("Invalid folder path")
+            continue
+          
+        elif asking == 2:
+          return asking
       except ValueError:
         print("please choose between 1 and 2")
 
 
 def main():
+  FS = FileSearching()
   while True:
     print("=========================================\nWELCOME TO FILE SEARCH TOOL\n=========================================\n")
 
-    f_s = file_searching()
-    ask = f_s.ask_user()
+    ask = FS.AskUser()
 
     if ask == 2:
       print("Thankyou for considering us")
